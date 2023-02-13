d6d8554f2eab602c3677afaf17381dceaaba7cb2
maykonjuso <maykola1331@gmail.com>
2023-02-13 15:23:45 -0300

    first commit

--- src/components/optiton/index.jsx
+++ src/components/optiton/index.jsx
@@ -2,8 +2,21 @@ import "./style";
 import { Button } from "./style";
 import { useContext } from "react";
 import { QuizContext } from "../../context/quiz";
+import { useState } from "react";
 
 export default function App({ option, selectOption, answer }) {
   const [quizState, dispatch] = useContext(QuizContext);
-  return <Button onClick={() => selectOption()}>{option}</Button>;
+
+  const [color, setColor] = useState("#ceb8e9");
+
+  const handleClick = () => {
+    selectOption();
+    setColor("#84dbff");
+  };
+
+  return (
+    <Button onClick={handleClick} style={{ backgroundColor: color }}>
+      {option}
+    </Button>
+  );
 }
